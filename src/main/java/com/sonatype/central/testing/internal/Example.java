18424a581648a4ca72362756d8ff7e35b68f77fa
Amelia Keibler <akeibler@sonatype.com>
2023-10-03 15:51:56 -0400

    Deployed via OSSRH
    https://repo1.maven.org/maven2/com/sonatype/central/testing/internal/example_switching_between_ossrh_and_portal/0.0.1/

---
+++ src/main/java/com/sonatype/central/testing/internal/Example.java
@@ -0,0 +1,18 @@
+package com.sonatype.central.testing.internal;
+
+import java.util.Locale;
+
+/**
+ * Example documentation
+ */
+public class Example
+{
+  /**
+   * Example function
+   * @param input The text to be modified
+   * @return The modified text
+   */
+  public String example(String input) {
+    return input.toLowerCase(Locale.getDefault());
+  }
+}
